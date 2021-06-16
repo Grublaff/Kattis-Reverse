35ef8e096891d980dc51fbc96e75fefe8ecf81a7
Grubla <filip.aburg@gmail.com>
2021-06-16 13:25:36 +0200

    Add project files.

---
+++ Kattis Reverse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kattis_Reverse
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(Int32.Parse(Console.ReadLine()));
+            }
+            numbers.Reverse();
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
